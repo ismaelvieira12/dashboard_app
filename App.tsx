06eadc2298f607b1810e42bc3049b93d6c23ffb1
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-08-30 00:47:07 -0300

    updata

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+
 export default function App() {
   return (
     <>
-    <View style={styles.container}>
-      <Text>Ola Mundoo!!</Text>
-      <StatusBar style="auto" />
-    </View>
+     <div>TEste</div>
     </>
   );
 }
