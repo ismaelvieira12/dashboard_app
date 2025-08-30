8e6f2e6c654745b883c0e9c65afc9214b89f9668
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-08-30 13:57:30 -0300

    Criando o primeiro componente

--- App.tsx
+++ App.tsx
@@ -1,21 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import {Home} from './src/components/home/Home';
 
 
 export default function App() {
   return (
     <>
-     <div>TEste</div>
-    </>
+      <Home />
+      <StatusBar style="auto" />
+    </> 
+      
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
