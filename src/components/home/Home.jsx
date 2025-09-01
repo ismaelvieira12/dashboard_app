15ac9fe21e19e829385153e8196bdd297487ffef
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-08-31 23:17:34 -0300

    update

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,22 @@
 import { Text } from 'react-native';
-
+import { StyleSheet } from 'react-native';
 export const Home = () => {
   return (
-    <>
+    
       <Text>Home</Text>
-    </>
+    
   );
 }
+
+
+
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000000",
+    alignItems: "center",
+    justifyContent: "flex-start", // <- muda para alinhar no topo
+    padding: 20,
+  },  
+});
