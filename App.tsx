a98639c70962feaf0b0bcefc2dacdf2d24076419
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-09-02 01:12:57 -0300

    criando a estrutura de pastas

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
 import { Home } from './src/components/home/Home';
 import { WelcomeScreen } from './src/screens/WelcomeScreen';
+import LoginScreen from './src/screens/login/LoginScreen';
+import SingUpScreen from './src/screens/SingUpScreen';
 
 const Stack = createStackNavigator();
 
@@ -11,8 +13,11 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="WelcomeScreen">
-        <Stack.Screen name="WelcomeScreen" options={{headerShown: false}} component={WelcomeScreen} />
         <Stack.Screen name="Home" options={{headerShown: false}} component={Home} />
+        <Stack.Screen name="WelcomeScreen" options={{headerShown: false}} component={WelcomeScreen} />
+        <Stack.Screen name="LoginScreen" options={{headerShown: false}} component={LoginScreen} />
+        <Stack.Screen name="SingUpScreen" options={{headerShown: false}} component={SingUpScreen} />
+
       </Stack.Navigator>
       <StatusBar style="auto" />
     </NavigationContainer>
