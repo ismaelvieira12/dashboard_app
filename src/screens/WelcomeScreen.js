a6edbfba1bee32f1c2ab202856b8ac766d1089e8
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-09-01 23:47:34 -0300

    update

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function WelcomeScreen() {
+export  const WelcomeScreen = () => {
   return (
     <View>
       <Text>WelcomeScreen</Text>
