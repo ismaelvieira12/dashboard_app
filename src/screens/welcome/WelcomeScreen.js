f05a74566acd9b0993d54e2e18fb6708e5017cab
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-09-02 01:43:35 -0300

    Update

--- src/screens/welcome/WelcomeScreen.js
+++ src/screens/welcome/WelcomeScreen.js
@@ -14,4 +14,24 @@ export const styles = StyleSheet.create({
     marginLeft: 10,
     marginRight: 10,
   },
+
+  imagemInicio: {
+    width: "100%",
+    height: 460,
+    marginTop: 40,
+    marginLeft: 10,
+    marginRight: 10,
+    borderRadius: 20,
+    resizeMode: 'cover', // Mantém a proporção da imagem
+  },
+
+  buttonSingUp: {
+    backgroundColor: "#00bfff",
+    paddingVertical: 15,
+    paddingHorizontal: 60,
+    borderRadius: 10,
+    marginTop: 20,
+    width: "100%",
+    alignItems: "center",
+  },
 });
--- src/screens/welcome/WelcomeScreen.jsx
+++ src/screens/welcome/WelcomeScreen.jsx
@@ -1,10 +1,17 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React from 'react';
 import { styles } from '../welcome/WelcomeScreen';
 export  const WelcomeScreen = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Bem VIndo ao Finance!</Text>
+      <Image  source={require('../../assets/Imagem_inicio.jpg')} style={styles.imagemInicio} />
+
+      <View style={{width: '100%', alignItems: 'center'}}>
+        <TouchableOpacity style={styles.buttonSingUp}>
+          <Text >Sing Up</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
