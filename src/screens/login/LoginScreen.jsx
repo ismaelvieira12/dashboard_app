5c81192085c94bea460266a4c21cb91d7ae5b7ec
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-09-03 23:21:07 -0300

    Update

--- src/screens/login/LoginScreen.jsx
+++ src/screens/login/LoginScreen.jsx
@@ -7,13 +7,7 @@ export default function LoginScreen() {
   return (
     <View style={login.boxLogin}>
       <Text style={login.textLogin}>LoginScreen</Text>
-      npm install react-native-linear-gradient
-      <LinearGradient
-        colors={['#4c669f', '#3b5998', '#192f6a']}
-        style={{ flex: 1, justifyContent: 'center', alignItems: 'center', borderRadius: 5 }}  
-      >
-        <Text style={{ color: '#fff', fontSize: 20, fontWeight: 'bold' }}>Login with Gradient</Text>
-      </LinearGradient>
+      
     </View>
 
   )
