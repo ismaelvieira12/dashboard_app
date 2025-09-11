aca886cbbf6baca6f2cb7428731dd91ddd2e5c35
Ismael Nascimento <ismaelvieira12@outlook.com>
2025-09-10 21:59:42 -0300

    Update

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,34 +1,13 @@
-import React from 'react'
-import { AreaChart, Grid } from 'react-native-svg-charts'
-import * as shape from 'd3-shape'
- 
 import { View, Text, StyleSheet } from 'react-native';
 
-class AreaChartExample extends React.PureComponent {
-  render() {
-    const data = [50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80]
-
-    return (
-      <AreaChart
-          style={{ height: 200 }}
-          data={data}
-          contentInset={{ top: 30, bottom: 30 }}
-          curve={shape.curveNatural}
-          svg={{ fill: 'rgba(134, 65, 244, 0.8)' }}
-      >
-          <Grid />
-      </AreaChart>
-    )
-  }
-}
 export const Home = () => {
-  
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home</Text>
     </View>
   );
 };
+
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -44,4 +23,3 @@ export const styles = StyleSheet.create({
     marginTop: 70,
   },
 });
-
