e66e552bde146032f191aff90e3d7c063d298a04
Ismael Nascimento <ismaelvieira12@outlook.com>
2025-09-11 07:27:08 -0300

    update

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,1 +1,26 @@
 
+import { View, Text, StyleSheet } from 'react-native';
+
+export const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home</Text>
+    </View>
+  );
+};
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#272727",
+    alignItems: "center",
+    justifyContent: "flex-start", // Alinha no topo
+    padding: 20,
+  },
+  text: {
+    color: "#fff",  // Deixa o texto visível no fundo preto
+    fontSize: 22,
+    fontWeight: "bold",
+    marginTop: 70,
+  },
+});
