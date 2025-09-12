67b9afc410b2b0cb2cd22cb3ca1c668e002cfbce
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-09-11 21:27:22 -0300

    update

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,13 +2,31 @@
 import { View, Text, StyleSheet } from 'react-native';
 import {Sidbar} from '../sidbar/Sidbar'
 import { Colors } from '../../style/Styles';
+import { GraficoLinha } from '../graficos/GraficoLinha';
+import { VictoryChart, VictoryLine, VictoryBar, VictoryPie, VictoryTheme } from "victory-native";
 
 export const Home = () => {
   return (
-    <View style={styles.container}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Sidbar />
       <Text style={styles.text}>Home</Text>
-      
+      < GraficoLinha />
+
+         {/* Gráfico de Linhas */}
+      <VictoryChart theme={VictoryTheme.material}>
+        <VictoryLine
+          data={[
+            { x: 1, y: 2 },
+            { x: 2, y: 3 },
+            { x: 3, y: 5 },
+            { x: 4, y: 4 },
+            { x: 5, y: 7 }
+          ]}
+          style={{
+            data: { stroke: "#38A3A5", strokeWidth: 3 }
+          }}
+        />
+      </VictoryChart>
     </View>
   );
 };
