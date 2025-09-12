a499204e5d624a237d5a58640f00100952afd723
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-09-11 23:00:49 -0300

    update

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,9 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
     plugins: [
-      "react-native-reanimated/plugin", // precisa ser o último plugin sempre
+       'react-native-worklets/plugin',
     ],
   };
 };
-    
