926f97e80969ef1c7ee18d4bfb3f58e0707ddff5
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-09-16 20:09:02 -0300

    Update

--- src/components/Home/HomeScreen.js
+++ src/components/Home/HomeScreen.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { CartesianChart } from "victory-native";
+import { CartesianChart, Line } from "victory-native";
 
 const DATA = Array.from({ length: 31 }, (_, i) => ({
   day: i,
@@ -10,14 +10,15 @@ const DATA = Array.from({ length: 31 }, (_, i) => ({
 export const  HomeScreen = () => {
   return (
     <View>
-      <View style={{width: "10%", height: 350}}>
+      <View style={{width: "100%", height: 350}}>
+
         <CartesianChart data={DATA} xKey="day" yKeys={["highTmp"]}>
-          {/* 👇 render function exposes various data, such as points. */}
           {({ points }) => (
-            // 👇 and we'll use the Line component to render a line path.
+
             <Line points={points.highTmp} color="red" strokeWidth={3} />
           )}
         </CartesianChart>
+
       </View>
     </View>
   )
