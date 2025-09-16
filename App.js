2c56737680df7e3e5dde324dd95c494f7a66df0d
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-09-15 23:33:20 -0300

    Update

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import {WelcomeScreen} from './src/components/Welcome/WelcomeScreen';
+import { HomeScreen } from './src/components/Home/HomeScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <WelcomeScreen />
+      
+      <HomeScreen />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/components/Home/HomeScreen.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export const  HomeScreen = () => {
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+    </View>
+  )
+}
