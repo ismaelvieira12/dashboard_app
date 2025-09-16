a5a4adc054f9b95779e24515ea764ef67f7ff43c
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-09-16 20:06:08 -0300

    fazendo o prebuild

--- src/components/Home/HomeScreen.js
+++ src/components/Home/HomeScreen.js
@@ -1,10 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { CartesianChart } from "victory-native";
+
+const DATA = Array.from({ length: 31 }, (_, i) => ({
+  day: i,
+  highTmp: 40 + 30 * Math.random(),
+}));
 
 export const  HomeScreen = () => {
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <View style={{width: "10%", height: 350}}>
+         <CartesianChart data={DATA} xKey="day" yKeys={["highTmp"]} />
+      </View>
     </View>
   )
 }
