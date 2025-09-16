adf3d53178ba87f723a667ef81add246569bce54
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-09-16 20:41:25 -0300

    Criando as funções

--- src/components/Home/HomeScreen.js
+++ src/components/Home/HomeScreen.js
@@ -1,7 +1,8 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { graficos } from '../Home/Home';
-import { CartesianChart, Line } from "victory-native";
+import { useAnimatedProps } from 'react-native-reanimated';
+import { CartesianChart, Line, useChartPressState } from "victory-native";
 
 const DATA = [
   {day: new Date("2025-09-10").getTime(), price: 500},
@@ -9,17 +10,39 @@ const DATA = [
   {day: new Date("2025-09-12").getTime(), price: 600},
   {day: new Date("2025-09-13").getTime(), price: 720},
   {day: new Date("2025-09-14").getTime(), price: 430},
-  {day: new Date("2025-09-15").getTime(), price: 100},
+  {day: new Date("2025-09-15").getTime(), price: 10},
   {day: new Date("2025-09-16").getTime(), price: 980},
 
 ]
 
 export const  HomeScreen = () => {
+
+  // criando interação com o grafico
+  const { state, isActive} = useChartPressState({x, y: { price: 0 }})
+
+  // Criando a div de animaão para mostrar os valores na tela
+  const animetedText = useAnimatedProps(() => {
+    return {
+      Text: `R$ ${state.y.price.value.value.toFixed(2)}`,
+      defaultValue: ""
+    }
+  })
+
+  // para mostrar a Data na tela
+   const animetedData = useAnimatedProps(() => {
+    return {
+      Text: `R$ ${state.y.price.value.value.toFixed(2)}`,
+      defaultValue: ""
+    }
+  })
+
   return (
     <View style={graficos.containerGra}>
       <View style={{width: "100%", height: 350}}>
 
-        <CartesianChart data={DATA} xKey="day" yKeys={["price"]}>
+        <CartesianChart data={DATA} xKey="day" yKeys={["price"]}
+          chartPressState={state}
+        >
           {({ points }) => (
 
             <Line points={points.price} color="#FA4" strokeWidth={4} />
