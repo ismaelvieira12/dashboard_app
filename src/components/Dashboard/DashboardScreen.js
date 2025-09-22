a9a20fe73c6a2d9825db8729328f5c5568da8a0e
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-09-21 22:59:13 -0300

    Colocando os graficos no lugar

--- src/components/Dashboard/DashboardScreen.js
+++ src/components/Dashboard/DashboardScreen.js
@@ -1,39 +1,30 @@
-import {  Text, View } from 'react-native'
-import { Bar, CartesianChart } from "victory-native"
-import React from 'react'
-import { Dash } from './Dashboard'
+import { Text, View } from "react-native";
+import { CartesianChart, Bar } from "victory-native";
+import React from "react";
+import { Dash } from "./Dashboard";
 
 const data = Array.from({ length: 6 }, (_, index) => ({
-    // Starting at 1 for January
-    month: index + 1,
-    // Randomizing the listen count between 100 and 50
-    listenCount: Math.floor(Math.random() * (100 - 50 + 1)) + 50,
-}))
+  month: new Date(2025, index, 1).getTime(), // usar timestamp no eixo X
+  listenCount: Math.floor(Math.random() * (100 - 50 + 1)) + 50,
+}));
 
 export const DashboardScreen = () => {
   return (
     <View style={Dash.container}>
       <Text style={Dash.text}>DashboardScreen</Text>
 
-      <CartesianChart
-        data={data}
-        /**
-         * 👇 the xKey should map to the property on data of you want on the x-axis
-         */
-        xKey="month"
-        /**
-         * 👇 the yKey is an array of strings that map to the data you want
-         * on the y-axis. In this case we only want the listenCount, but you could
-         * add additional if you wanted to show multiple song listen counts.
-         */
-        yKeys={["listenCount"]}>
-        {({ points, chartBounds }) => (
-          <Bar
-            chartBounds={chartBounds}  // 👈 chartBounds is needed to know how to draw the bars
-            points={points.listenCount} // 👈 points is an object with a property for each yKey
-          />
-        )}
-      </CartesianChart>
+      <View style={{ width: "100%", height: 300 }}>
+        <CartesianChart data={data} xKey="month" yKeys={["listenCount"]}>
+          {({ points, chartBounds }) => (
+            <Bar
+              chartBounds={chartBounds}
+              points={points.listenCount}
+              barWidth={20} // 👈 largura das barras
+              color="#4CAF50" // 👈 cor das barras
+            />
+          )}
+        </CartesianChart>
+      </View>
     </View>
-  )
-}
+  );
+};
