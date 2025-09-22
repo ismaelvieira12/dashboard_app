407c80ad4b97cba95b6ed9de591bade92c096121
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-09-21 22:54:11 -0300

    Colocando o grafico de anos

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native'
+export const Dash = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#02092fff', // Cor de fundo clara
+    },
+    text: {
+        fontSize: 20,
+        color: '#fffbfbff', // Cor do texto escura
+    },
+})
--- src/components/Dashboard/DashboardScreen.js
+++ src/components/Dashboard/DashboardScreen.js
@@ -1,12 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
+import {  Text, View } from 'react-native'
+import { Bar, CartesianChart } from "victory-native"
 import React from 'react'
+import { Dash } from './Dashboard'
+
+const data = Array.from({ length: 6 }, (_, index) => ({
+    // Starting at 1 for January
+    month: index + 1,
+    // Randomizing the listen count between 100 and 50
+    listenCount: Math.floor(Math.random() * (100 - 50 + 1)) + 50,
+}))
 
 export const DashboardScreen = () => {
   return (
-    <View>
-      <Text>DashboardScreen</Text>
+    <View style={Dash.container}>
+      <Text style={Dash.text}>DashboardScreen</Text>
     </View>
   )
 }
-
-const styles = StyleSheet.create({})
