2ca061b0f5c925a38cefb3bd03d8d3129814f897
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-10-11 01:55:23 -0300

    update

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -4,10 +4,9 @@ export const Dash = StyleSheet.create({
 
     container: {
         flex: 1,
-        // justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#02092fff', // Cor de fundo clara
-        // backgroundColor: '#e9eae9ff', // Cor de fundo clara
+        // backgroundColor: '#02092fff', // Cor de fundo clara
+        backgroundColor: '#e9eae9ff', // Cor de fundo clara
         height: '100%',
         position: 'relative',
     },
